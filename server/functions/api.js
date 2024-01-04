1b6d91aea505e42e8c2c75f7653d4fe80f0291e8
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2024-01-04 23:24:45 +0900

    [Feat] Netlify 환경 설정
    - functions, lambda 설정

---
+++ server/functions/api.js
@@ -0,0 +1,51 @@
+// 외부 모듈
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import "express-async-errors";
+import helmet from "helmet";
+import morgan from "morgan";
+import serverless from "serverless-http";
+
+// 내부 모듈
+import { config } from "./config.js";
+import { db } from "./db/database.js";
+import authRouter from "./router/auth.js";
+import gamesRouter from "./router/games.js";
+import profilesRouter from "./router/profiles.js";
+// import { csrfCheck } from "./middleware/csrf.js";
+import rateLimit from "./middleware/rate-limiter.js";
+
+const app = express();
+
+const corsOption = {
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors(corsOption));
+app.use(morgan("tiny"));
+app.use(rateLimit);
+
+// app.use(csrfCheck);
+app.use("/.netlify/functions/api/profiles", profilesRouter);
+app.use("/.netlify/functions/api/auth", authRouter);
+app.use("/.netlify/functions/api/games", gamesRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+
+db.getConnection().then((connection) => console.log);
+app.listen(config.port);
+
+module.exports.handler = serverless(app);
