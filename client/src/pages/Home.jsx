491f3d544919d44b80505db1d8b5af3a5a8c47be
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2024-01-09 14:00:07 +0900

    DOCS: 호스팅 서버 슬립 모드 관련 문구 추가
    - 호스팅 서버가 자동으로 슬립 모드로 전환된다고 안내하는 문구 추가

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -45,6 +45,12 @@ const Home = ({ onVerify, onCodeCheck, onSignUp, onLogin }) => {
         </div>
       </div>
       <span className="info-text">로그인 하신 뒤 이용해주세요</span>
+      <h3>
+        호스팅 서버가 자동으로 슬립 모드로 전환되기 때문에
+        <br />첫 요청을 보낼 때 시간이 조금 소요될 수 있습니다
+        <br />
+        로그인 버튼 클릭 후 1분 정도 지난 뒤 다시 로그인 시도 부탁드립니
+      </h3>
       <span className="info-text">- 테스트 계정 -</span>
       <span style={{ color: "#999", fontSize: "0.9rem" }}>
         아이디: test@test.com
